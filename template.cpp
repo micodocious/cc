19d73d65d791336f85f24540407140c60b8614f8
Mico Santiago <Micomiguelsantiago@gmail.com>
2025-02-18 20:28:54 -0800

    first commit

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  
+  // Copy all input to output as is
+  string line;
+  while (getline(cin, line)) {
+      cout << line << "\n";
+  }
+
+}
