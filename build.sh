3a2e64ad16f0e79d2e1e2f261ae2b33b6df5f703
Mico Santiago <Micomiguelsantiago@gmail.com>
2025-02-26 17:37:41 -0800

    template updated

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -std=c++11 -O2 -Wall test.cpp -o test
-./test.exe
+g++ -std=c++11 -O2 -Wall template.cpp -o template
+./template.exe
--- template.cpp
+++ template.cpp
@@ -3,15 +3,49 @@
 using namespace std;
 
 int main() {
+  //make the code run faster
   ios::sync_with_stdio(0);
   cin.tie(0);
+  /* 
+  //--------------------- I/O
+  //read input/output files
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
-  
-  // Copy all input to output as is
-  string line;
-  while (getline(cin, line)) {
-      cout << line << "\n";
+  //basic i/o
+  int a = 123, b = 456;
+  string x ="monkey";
+  cout << a << "" << b << "" << x << "\n";
+  //read
+  int a,b;
+  scanf("%d %d", &a, &b);
+  //print
+  int a = 123, b = 456;
+  printf("%d %d\n", a, b);
+  //read whole line
+  string s;
+  getline(cin, s);
+  //when the amount of data is unknown
+  while (cin>>x){
+    //code
   }
+  //----------------------- NUMBERS
+  //32 bit
+  int z = 10;
+  //64 bit
+  long long x = 123456789123456789LL;  
+  long long x = 1;
+  for (int i=2;i<=n;i++){
+    x=(x*i)%m;
+    printf("%llu\n",x);
+  }
+  cout << x%m << "\n";
+  // make sure there are no negative nums in remainder
+  x = x%ml
+  if (x < 0) x += m;
+  */
+  //----------------------- FLOATING POINT NUMBERS
+  // 64 bit double and 80 bit long double (more accurate)
+  printf("%.9f\n",x);
+
 
 }
